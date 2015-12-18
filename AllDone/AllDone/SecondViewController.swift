ce0caa5e17085865699f1bd472779122b7a4f4c6
majin <majin@xiaochuankeji.cn>
2015-12-18 20:18:22 +0800

    init

---
+++ AllDone/AllDone/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AllDone
+//
+//  Created by lieyunye on 12/18/15.
+//  Copyright © 2015 lieyunye. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
