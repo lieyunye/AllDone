2b2d5c4de576832a792430202870213d3f7b977b
majin <majin@xiaochuankeji.cn>
2017-04-18 08:49:44 +0800

    sync

---
+++ AllDone/AllDone/Controllers/ADUserInfoHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  ADUserInfoHeaderView.swift
+//  AllDone
+//
+//  Created by lieyunye on 4/18/17.
+//  Copyright © 2017 lieyunye. All rights reserved.
+//
+
+import UIKit
+
+class ADUserInfoHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
